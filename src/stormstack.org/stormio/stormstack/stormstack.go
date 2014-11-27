24b9532dbe3a3eb53647b588a0772897627e824d
Sivaprasath Busa <sivaprasathb@calsoftlabs.com>
2014-11-27 08:41:42 -0800

    Fixed a typo

--- src/stormstack.org/stormio/stormstack/stormstack.go
+++ src/stormstack.org/stormio/stormstack/stormstack.go
@@ -126,7 +126,7 @@ func RegisterStormAgent(arq *persistence.AssetRequest, entityId string) (err err
 	arq.AgentId = resp.Id
 	arq.SerialKey = entityId
 	log.Debugf("[areq %s][res %s] Registered Agent with stormtracker %v", arq.Id, arq.ResourceId, arq.AgentId)
-	Register the agent in Stormlight default domain
+	// Register the agent in Stormlight default domain
 	err = DomainAddAgent(arq)
 	if err != nil {
 		log.Debugf("[areq %s][res %s] Error in Adding agent %v to default domain %v", arq.Id, arq.ResourceId, arq.AgentId, arq.ControlProvider.DefaultDomainId)
